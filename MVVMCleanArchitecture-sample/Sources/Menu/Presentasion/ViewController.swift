250eba5d6e1ca49ee161700f4680fb44c37cab4b
osakamiseri <str3tech@gmail.com>
2021-08-04 23:00:00 +0900

    主なUI作成

--- MVVMCleanArchitecture-sample/Sources/Menu/Presentasion/ViewController.swift
+++ MVVMCleanArchitecture-sample/Sources/Menu/Presentasion/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxRelay
 
 final class ViewController: UIViewController {
 
@@ -15,9 +18,39 @@ final class ViewController: UIViewController {
         }
     }
 
+    private let viewModel = ViewModel()
+    private let disposeBag = DisposeBag()
+    // private let adapter = RxTableViewArrayAdaptor()
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        hidesBottomBarWhenPushed = true
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    private func setupUI() {
+        navigationItem.title = "サンプル"
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .done, target: nil, action: nil)
+        let closeItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(ViewController.didTapClose))
+        navigationItem.setLeftBarButton(closeItem, animated: true)
+    }
+
+    private func bindViewModel() {
+
+    }
+
+    @objc private func didTapClose() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setToolbarHidden(true, animated: false)
     }
 
 }
